072cda638c941d60539ce94d257bb013c1094340
Przemysław Wojenka <p.wojenka@361.sh>
2025-02-17 18:56:50 +0100

    deployment setup

--- frontend/config/supabase.ts
+++ frontend/config/supabase.ts
@@ -1,7 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
-import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: false, // We're handling session persistence ourselves
+    autoRefreshToken: true,
+    detectSessionInUrl: false
+  }
+}); 
